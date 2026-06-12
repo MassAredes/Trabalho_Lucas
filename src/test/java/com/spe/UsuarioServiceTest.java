23686da0d21f47a42cd2cc6d9ae43b4fd45b56b7
jclclick <jcjcesarjcjc@gmail.com>
2026-06-11 21:27:17 -0300

    Update UsuarioServiceTest.java

--- src/test/java/com/spe/UsuarioServiceTest.java
+++ src/test/java/com/spe/UsuarioServiceTest.java
@@ -32,16 +32,16 @@ class UsuarioServiceTest {
     @Test
     void cadastroEAutenticacaoOk() {
 
-        // Cria o serviço de usuários utilizando um repositório em memória. */
+        //** Cria o serviço de usuários utilizando um repositório em memória. */
         UsuarioService s = new UsuarioService(new InMemoryUsuarioRepository());
 
-        // Cadastra um novo usuário do tipo gerente. */
+        //** Cadastra um novo usuário do tipo gerente. */
         s.cadastrar("ana", "senha123", PapelUsuario.GERENTE);
 
-        // Verifica se a autenticação com a senha correta funciona. */
+        //** Verifica se a autenticação com a senha correta funciona. */
         assertTrue(s.autenticar("ana", "senha123").isPresent());
 
-        // Verifica se a autenticação com senha incorreta falha. */
+        //** Verifica se a autenticação com senha incorreta falha. */
         assertFalse(s.autenticar("ana", "errada").isPresent());
     }
 
