4eeeb9fe0df7e30ab8a9d3c038212f125f6130e6
Ramez Marques <164257539+Ramez1006@users.noreply.github.com>
2026-06-15 19:51:26 -0300

    commit do arquivo em loco

--- src/main/java/com/spe/exception/DominioException.java
+++ src/main/java/com/spe/exception/DominioException.java
@@ -1,5 +1,6 @@
 package com.spe.exception;
 
+// Excecao usada quando alguma regra do negocio e quebrada.
 public class DominioException extends RuntimeException {
     public DominioException(String msg) { super(msg); }
 }
